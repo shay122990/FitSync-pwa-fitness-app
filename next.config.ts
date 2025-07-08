6192a641ff11208ed21b412ed1cc85043a38f38c
shay asanova <moeshakasanova@gmail.com>
2025-07-08 14:16:08 +0400

    icons and structure added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import withPWA from 'next-pwa';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {},
 };
 
-export default nextConfig;
+export default withPWA({
+  ...nextConfig,
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
